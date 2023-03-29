4196fad10d92310c9427f150f3753b54179fadfe
anga95 <andreas.gabrielsson@live.se>
2023-03-29 18:57:43 +0200

    ready for redovisning 3.0

--- file_handling.h
+++ file_handling.h
@@ -7,7 +7,7 @@
 #include "Quiz_card.h"
 #include "Quiz.h"
 #include <QString>
-const QString FILE_PATH = "/Users/andreas/Documents/MIUN/programmeringsmetodik/qt/Flashcards/test.txt";
+const QString FILE_PATH = "/Users/andreas/Documents/MIUN/programmeringsmetodik/qt/Flashcards/data.txt";
 void parse_file(QVector<Quiz_card> &questions, QString input_file = FILE_PATH);
 void save_card_to_file(const Quiz_card& card);
 void save_quiz_to_file(const Quiz& quiz);
